4a9942ca0e5e2a306fbf3b79137cbc943a06c12a
Sen'app Studios <senappstudios@gmail.com>
2020-10-08 23:08:21 +0200

    Update Cell.js

--- scripts/Cell.js
+++ scripts/Cell.js
@@ -13,23 +13,23 @@ function Cell(i, j) {
   if (Math.random(1) < wallModifier) {
     this.wall = true;
   }
+
   this.show = function (fill = "white") {
     ctx.beginPath();
     ctx.strokeStyle = "black";
     ctx.lineWidth = 1;
 
     if (renderMode == 0) ctx.rect(i * w, j * h, w - 1, h - 1);
-    else if (renderMode == 1)
-      ctx.arc(i * w + w / 2, j * h + h / 2, (w + h) / 4, 0, Math.PI * 2);
-    else if (renderMode == 2 && this.wall)
-      ctx.arc(i * w + w / 2, j * h + h / 2, (w + h) / 8, 0, Math.PI * 2);
-    if ((fill && renderMode != 2) || this.wall) {
-      ctx.fillStyle = fill;
-      if (this.wall) ctx.fillStyle = "black";
+    else if (renderMode == 1) ctx.arc(i * w + w / 2, j * h + h / 2, (w + h) / 4, 0, Math.PI * 2);
+    else if (renderMode == 2 && this.wall) ctx.arc(i * w + w / 2, j * h + h / 2, (w + h) / 8, 0, Math.PI * 2);
+
+    if (fill && renderMode != 2 || this.wall) {
+      ctx.fillStyle = this.wall ? "black" : fill;
       ctx.fill();
     }
     ctx.stroke();
   };
+
   this.addNeighbours = function (grid) {
     if (this.i < cols - 1) {
       this.neighbours.push(grid[this.i + 1][this.j]);
